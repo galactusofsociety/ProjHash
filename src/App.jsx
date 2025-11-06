7f847186afd6ab0ef239951586eae22170974f23
galctusofsociety <piyushonly786@gmail.com>
2025-11-06 17:06:45 +0530

    hkfr

--- src/App.jsx
+++ src/App.jsx
@@ -646,7 +646,7 @@ const App = () => {
 
   return (
     <div style={styles.container}>
-      <h1 style={styles.title}>⚡ HASHING VISUALIZER ⚡</h1>
+      <h1 style={styles.title}> 💖 HASHING VISUALIZER 💖</h1>
       <div style={styles.controls}>
         <div style={styles.controlGroup}>
           <label style={styles.label}>Table Size:</label>
